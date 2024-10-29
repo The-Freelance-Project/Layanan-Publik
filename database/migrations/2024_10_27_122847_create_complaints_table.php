01352eee080fdde4e971ca9a8b325133c45101e6
Ilham Sidik <ilhamsidikc@gmail.com>
2024-10-29 12:54:42 +0700

    add complaint

---
+++ database/migrations/2024_10_27_122847_create_complaints_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id')->references('id')->on('users');
+            $table->foreignUuid('category_id')->nullable();
+            $table->string('title', 200);
+            $table->text('description');
+            $table->enum('status', ['pending', 'in_progress', 'resolved', 'rejected', 'canceled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
