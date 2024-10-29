b7b61424fe11c3f2487440810e834d68af37202c
Ilham Sidik <ilhamsidikc@gmail.com>
2024-10-29 12:55:36 +0700

    update the view

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,8 +1,42 @@
-User Dashboard
 
-{{ Auth::user()->name }}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }}</title>
 
+    <!-- Set TailwindCss -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
 
-<!-- untuk logout gunakan ini -->
+<!-- Set Alert biar muncul kalo error -->
+@include('components.alert')
 
-<a href="{{ route('logout') }}">Logout</a>
+<!-- ambil data user -->
+ @php
+ $user = Auth::user(); 
+ @endphp
+
+<!-- Menampilkan data user -->
+ {{ $user->name }}
+ <br>
+ {{ $user->email }}
+
+ <br>
+ <br>
+
+ <!-- Navigasi -->
+ <ul>
+    <li>
+        <a href="{{ route('complaint') }}">Complaint List</a>
+    </li>
+    <li>
+        <!-- untuk logout gunakan ini -->
+        <a href="{{ route('logout') }}">Logout</a>
+    </li>
+ </ul>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ResponseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,6 +24,17 @@ Route::middleware(['auth', 'user'])->prefix('user')->group(function (){
 
     Route::controller(DashboardController::class)->group(function(){
         Route::get('dashboard', 'user_dashboard')->name('user.dashboard');
+    });
+
+    Route::controller(ComplaintController::class)->prefix('complaints')->group(function(){
+        Route::get('list', 'complaint_list')->name('complaint');
+        Route::get('view/{id}', 'complaint_view')->name('complaint.view');
+        Route::get('form', 'complaint_form')->name('complaint.form');
+        Route::post('add', 'complaint_add')->name('complaint.add');
+    });
+
+    Route::controller(ResponseController::class)->prefix('responses')->group(function(){
+        Route::post('add', 'response_add')->name('response.add');
     });
 
 });
