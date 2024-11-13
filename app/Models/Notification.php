92db0a09d84304b9e705932f9e41ad4c681150b8
Ilham Sidik <ilhamsidikc@gmail.com>
2024-11-13 14:45:50 +0700

    add chat and notification Models

---
+++ app/Models/Notification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Notification extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'text',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
