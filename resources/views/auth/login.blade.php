188bbafed320ebea050334c863c103d29f644088
Ilham Sidik <ilhamsidikc@gmail.com>
2024-11-22 20:27:20 +0700

    create new feature login by google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ env('APP_NAME') }}</title>
     <!-- Include Tailwind CSS -->
+    <link rel="icon" type="image/x-icon" href="/logo.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -48,7 +49,44 @@
         </form>
 
         <p class="text-center text-gray-600 mt-6">Belum punya akun? <a href="register"
-                class="text-red-600 hover:text-red-800 font-medium">Daftar</a></p>
+            class="text-red-600 hover:text-red-800 font-medium">Daftar</a></p>
+
+        <div class="w-full flex justify-center items-center mt-3">
+            <div class="flex-1 border-b border-gray-500"></div>
+            <div class="mx-2 text-gray-600">atau</div>
+            <div class="flex-1 border-b border-gray-500"></div>
+        </div>
+
+        <a href="{{ route('redirect') }}" class="mt-4 w-full flex justify-center">  
+            <button
+                aria-label="Sign in with Google"
+                class="flex items-center gap-3 bg-google-button-blue rounded-full p-0.5 pr-4 transition-colors duration-300 hover:bg-google-button-blue-hover"
+                >
+                <div class="flex items-center justify-center bg-white w-9 h-9 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
+                    <title>Sign in with Google</title>
+                    <desc>Google G Logo</desc>
+                    <path
+                        d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                        class="fill-google-logo-blue"
+                    ></path>
+                    <path
+                        d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                        class="fill-google-logo-green"
+                    ></path>
+                    <path
+                        d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                        class="fill-google-logo-yellow"
+                    ></path>
+                    <path
+                        d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                        class="fill-google-logo-red"
+                    ></path>
+                    </svg>
+                </div>
+                <span class="text-sm text-white tracking-wider">Login dengan akun Google</span>
+            </button>
+        </a>
     </div>
 </body>
 
